e0958bfc9914b9d577994f7036ffa37a17c49a02
Amanda <amanda.kaijanen@gmail.com>
2021-11-29 13:36:17 +0200

    add new projects

---
+++ DirectorieaAndFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DirectorieaAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\AmandaAiku\samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+
+           
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{ rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{ rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+
+            
+
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myShoppingList = new List<string>();
+
+            Console.WriteLine(myShoppingList.Count);
+
+            myShoppingList.Add("milk");
+            myShoppingList.Add("potato");
+            myShoppingList.Add("apple");
+            myShoppingList.Add("coffee");
+
+            Console.WriteLine(myShoppingList.Count);
+
+            Console.WriteLine(myShoppingList[0]);
+            Console.WriteLine(myShoppingList[1]);
+            Console.WriteLine(myShoppingList[2]);
+
+            Console.WriteLine(myShoppingList.Count);
+
+            foreach (string wish in myShoppingList)
+            {
+                Console.WriteLine(wish);
+            }
+        }
+    }
+}
